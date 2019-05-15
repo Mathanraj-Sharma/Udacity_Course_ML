3a29cd257e3b823c9726c0c660450f35fba673dc
MR <rvmmathanraj@gmail.com>
2019-05-15 16:18:08 +0530

    first commit

---
+++ svm/svm_author_id.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+from sklearn import svm
+from sklearn import metrics
+import collections 
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+#########################################################
+### your code goes here ###
+#incearing C will increase the model complexity but increases the accuracy. It will increases the training and prediction time too. (learns all data points as possible)
+clf = svm.SVC(kernel = 'rbf', C = 10000)
+#features_train = features_train[:len(features_train)//100] 
+#labels_train = labels_train[:len(labels_train)//100] 
+clf.fit(features_train, labels_train)
+
+predict = clf.predict(features_test)
+print("Accuracy: ",metrics.accuracy_score(labels_test, predict))
+
+#print("for 10th: ",predict[10])
+#print("for 26th: ",predict[26])
+#print("for 50th: ",predict[50])
+print(collections.Counter(predict))
+#########################################################
+
+
